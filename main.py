1b1841ed903c283d7fba115bf54f057f8beab466
vovparya <vovparya@gmail.com>
2024-06-23 16:48:47 +0300

    Module No. 1. GIT practice. Basic Data Structures

---
+++ main.py
@@ -0,0 +1,30 @@
+# Задача «Длина слова».
+a = 'Hi!'
+print(len(a))
+
+# Задача «Суммы и разности».
+first = 42
+second = 43
+summa = first + second
+diff = second - first
+print(summa)
+print(diff)
+
+# Задача «Среднее арифметическое».
+first = 42
+second = 43
+third = 44
+mean = (first + second + third) / 3
+print(mean)
+
+# Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ',', first_string)
+
+# Задача «Сложная формула».
+a = 1
+b = 2
+c = 3
+f = ((a * b) + (a * c)) ** 3 / 2
+print(f)
