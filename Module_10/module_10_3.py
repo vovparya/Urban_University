67eabb12414c9ed6dab999557b39648ceec6bb16
vovparya <vovparya@gmail.com>
2024-09-09 23:51:49 +0300

    Задания модуля 10. Домашнее задание по теме "Блокировки и обработка ошибок"

--- Module_10/module_10_3.py
+++ Module_10/module_10_3.py
@@ -90,27 +90,28 @@ class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = threading.Lock()
-        self.condition = threading.Condition(self.lock)
 
     def deposit(self):
         for _ in range(100):
             amount = random.randint(50, 500)
-            with self.condition:  # Используем condition для синхронизации
+            with self.lock:
                 self.balance += amount
                 print(f"Пополнение: {amount}. Баланс: {self.balance}")
-                self.condition.notify_all()  # Уведомляем другие потоки
+                if self.balance >= 500 and self.lock.locked():
+                    self.lock.release()  # Открываем замок, если баланс >= 500
             time.sleep(0.001)
 
     def take(self):
         for _ in range(100):
             amount = random.randint(50, 500)
             print(f"Запрос на {amount}")
-            with self.condition:
-                while amount > self.balance:  # Ждем, пока не будет достаточно денег
+            with self.lock:  # Захватываем блокировку перед снятием
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
+                else:
                     print("Запрос отклонён, недостаточно средств")
-                    self.condition.wait()
-                self.balance -= amount
-                print(f"Снятие: {amount}. Баланс: {self.balance}")
+                    self.lock.acquire()  # Блокируем поток, если не хватает средств
             time.sleep(0.001)
 
 
@@ -125,7 +126,7 @@ th2 = threading.Thread(target=bk.take)
 th1.start()
 th2.start()
 
-# Ожидаем завершения обоих потоков
+# Ожидаем завершения потоков
 th1.join()
 th2.join()
 
