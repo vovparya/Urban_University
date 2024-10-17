8ba0ce8dee0087c1e8c3d70c42f75995789b47ce
vovparya <vovparya@gmail.com>
2024-10-17 15:53:59 +0300

    Задания модуля 13. Домашнее задание по теме "Методы отправки сообщений".

--- Module_13/module_13_3.py
+++ Module_13/module_13_3.py
@@ -16,7 +16,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-API_KEY = '**********************************************'
+API_KEY = '***********************************************'
 bot = Bot(token=API_KEY)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
